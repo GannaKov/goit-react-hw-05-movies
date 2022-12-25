077e926cedd0afa44e77da1de63a000aaddfca4c
GannaKov <zlatta2000@gmail.com>
2022-12-25 21:20:12 +0200

    app start

---
+++ src/components/App/App.jsx
@@ -0,0 +1,36 @@
+import { Routes, Route} from "react-router-dom";
+import React from 'react';
+//import React, { useState } from 'react';
+import { GlobalStyle } from 'CreateGlobalStyle';
+import { Home } from "../../pages/Home";
+import { Link,Container, Header} from "./App.styled";
+
+
+export const App = () => {
+ 
+
+  return (
+    <Container>
+      <GlobalStyle /> 
+      <Header> 
+        <nav>
+      <Link to="/" end>
+          Home
+        </Link>
+        <Link to="/movies">Movies</Link>
+</nav>
+</Header> 
+     
+      <Routes>
+    <Route path="/" element={<Home />}/>
+    <Route path="/movies" element={<div>Movies</div>} />
+       
+        <Route path="*" element={<div>NotFound </div>} />
+      </Routes>
+   </Container>
+  );
+};
+// https://api.themoviedb.org/3/movie/550?api_key=894ef72300682f1db325dae2afe3e7e2
+//{ <Route path="/" element={<Home />} />
+//<Route path="/about" element={<About />} />
+//<Route path="/products" element={<Products />} /> }
