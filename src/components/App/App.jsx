c9a0a7e7b266449851da4b5b91f95edb8033c84a
GannaKov <zlatta2000@gmail.com>
2022-12-29 00:07:31 +0200

    gl style

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route} from "react-router-dom";
-// import React from 'react';
-import { Toaster } from 'react-hot-toast'
-//import React, { useState } from 'react';
 import { GlobalStyle } from 'CreateGlobalStyle';
+import { Toaster } from 'react-hot-toast'
+
+
 import { SharedLayout } from "components/SharedLayout/SharedLayout";
 import { Home } from "../../pages/Home";
 import { Movies } from "pages/Movies";
-import { Link,Container, Header} from "./App.styled";
+
 import { FilmDetails } from "pages/MovieDetails";
 import { FilmCast } from "components/FilmCast/FilmCast";
 import { FilmReviews } from "components/Filmreviews/FilmReviews";
@@ -16,18 +16,32 @@ export const App = () => {
  
 
   return (
-  <Routes>
-    <Route path="/" element={<SharedLayout />}>
-      <Route index element={<Home />} />
-      <Route path="movies" element={<Movies/>}/>
-      <Route path="movies/:id" element={<FilmDetails />}>
-         <Route path="cast" element={<FilmCast/>} />
-         <Route path="reviews" element={<FilmReviews/>} /> 
-      </Route>   
-     <Route path="*" element={<div>NotFound </div>} /> 
-    </Route>
- </Routes>
+   <div> <Routes>
+   <Route path="/" element={<SharedLayout />}>
+     <Route index element={<Home />} />
+     <Route path="movies" element={<Movies/>}/>
+     <Route path="movies/:id" element={<FilmDetails />}>
+        <Route path="cast" element={<FilmCast/>} />
+        <Route path="reviews" element={<FilmReviews/>} /> 
+     </Route>   
+    <Route path="*" element={<div>NotFound </div>} /> 
+   </Route>
+</Routes>
+<Toaster toastOptions={{
+     
+ error: {duration: 3000,
+   style: { border: '1px solid red',
+   padding: '16px',
    
+     minWidth: '450px',
+    
+   },
+ },
+}} />
+<GlobalStyle />
+</div>
+ 
+
   );
 };
 // https://api.themoviedb.org/3/movie/550?api_key=894ef72300682f1db325dae2afe3e7e2
